d3bdec5fe635ef3692144e66e4e7a84b2403612a
MD <maksim.dudarev@gmail.com>
2020-04-08 13:34:42 +0300

    add crud

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,85 @@
 <template>
-  <div id="app">
+  <div id="app" class="small-container">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>Employees</h1>
+
+    <employee-table
+      :employees="employees"
+      @delete:employee="deleteEmployee"
+      @edit:employee="editEmployee"
+    />
+    <employee-form @add:employee="addEmployee" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    EmployeeTable,
+    EmployeeForm,
+  },
+  data() {
+    return {
+      employees: [],
+    }
+  },
+  mounted() {
+    this.getEmployees()
+  },
+  methods: {
+    async getEmployees() {
+      try {
+        const response = await fetch('http://formulaone-dev.us-west-2.elasticbeanstalk.com:8000/api/travellers')
+        const data = await response.json()
+        this.employees = data
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async addEmployee(employee) {
+      try {
+        const response = await fetch('http://formulaone-dev.us-west-2.elasticbeanstalk.com:8000/api/travellers', {
+          method: 'POST',
+          body: JSON.stringify(employee),
+          headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        })
+        const data = await response.json()
+        this.employees = [...this.employees, data]
+      } catch (error) {
+        console.error(error)
+      }
+    },    
+    async deleteEmployee(id) {
+      try {
+        await fetch(`http://formulaone-dev.us-west-2.elasticbeanstalk.com:8000/api/travellers/${id}`, {
+          method: "DELETE"
+        });
+        this.employees = this.employees.filter(employee => employee.id !== id);
+      } catch (error) {
+        console.error(error);
+      }
+    },    
+    async editEmployee(id, updatedEmployee) {
+      try {
+        const response = await fetch(`http://formulaone-dev.us-west-2.elasticbeanstalk.com:8000/api/travellers/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify(updatedEmployee),
+          headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        })
+        const data = await response.json()
+        this.employees = this.employees.map(employee => (employee.id === id ? data : employee))
+      } catch (error) {
+        console.error(error)
+      }
+    },
   }
 }
 </script>
@@ -24,5 +92,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
 }
 </style>
