6a73f9e477187fdb2ca77f19752c26686a7ac5df
MD <maksim.dudarev@gmail.com>
2020-04-10 11:06:26 +0300

    add router options

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
